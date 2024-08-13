19341e254e72898366ae8c13adc11e1aa473103b
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-08-13 17:44:34 +0900

    wip carousel

---
+++ src/routes/Svqk.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Carousel } from 'flowbite-svelte';
+
+    export const images = [
+    {
+      alt: 'jeg',
+      src: '/jpa-entity-generator.svg',
+      title: 'jeg'
+    },
+    {
+      alt: 'csv',
+      src: '/batch-translator.svg',
+      title: 'csv'
+    },
+  ];
+
+
+</script>
+
+<div >
+  <Carousel {images} duration="3000" />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import flowbitePlugin from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 };
