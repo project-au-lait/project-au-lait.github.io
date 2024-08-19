74c0bd72d7a1c5db01d5a9cedfef01ca9f611c54
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-08-19 17:12:43 +0900

    wip svqk lp

--- src/routes/Svqk.svelte
+++ src/routes/Svqk.svelte
@@ -1,7 +1,8 @@
 <script>
-  import { Carousel, Tabs, TabItem } from 'flowbite-svelte';
+  import { Carousel, P, Tabs, TabItem } from 'flowbite-svelte';
+  // import { t } from '$lib/translations';
 
-  export const images = [
+  const images = [
     {
       alt: 'jeg',
       src: '/jpa-entity-generator.svg',
@@ -13,13 +14,55 @@
       title: 'csv',
     },
   ];
+
+  const command = {
+    win: `
+chcp 65001
+
+mvn archetype:generate ^
+  -DarchetypeGroupId=dev.aulait.svqk ^
+  -DarchetypeArtifactId=svqk-archetype-refimpl ^
+  -DarchetypeVersion=0.8 ^
+  -DgroupId=my-groupId ^
+  -DartifactId=my-artifactId
+
+cd my-artifactId
+
+mvnw install -T 1C -P setup
+
+code my-artifactId.code-workspace
+    `,
+    mac: `
+mvn archetype:generate \\
+  -DarchetypeGroupId=dev.aulait.svqk \\
+  -DarchetypeArtifactId=svqk-archetype-refimpl \\
+  -DarchetypeVersion=0.8 \\
+  -DgroupId=my-groupId \\
+  -DartifactId=my-artifactId
+
+cd my-artifactId
+
+./mvnw install -T 1C -P setup
+
+code my-artifactId.code-workspace
+    `,
+  };
 </script>
 
-<Tabs>
-  <TabItem open title="Windows">mvnw</TabItem>
-  <TabItem title="Mac">./mvnw</TabItem>
-</Tabs>
+<div class="container mx-auto lg:w-4/5">
+  <P size="6xl">SVQK</P>
+  <P size="4xl">Features</P>
+  <P size="4xl">Quick Start</P>
+  <Tabs>
+    <TabItem open title="Windows">
+      <pre><code>{command.win}</code></pre>
+    </TabItem>
+    <TabItem title="Mac">
+      <pre><code>{command.mac}</code></pre>
+    </TabItem>
+  </Tabs>
 
-<div>
-  <Carousel {images} duration="3000" />
+  <div>
+    <Carousel {images}  duration="5000" />
+  </div>
 </div>
