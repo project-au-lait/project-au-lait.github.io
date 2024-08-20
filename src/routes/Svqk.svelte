8eee13b4bbbb431a5aed14ea69826cbf121285dc
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-08-20 17:07:51 +0900

    wip

--- src/routes/Svqk.svelte
+++ src/routes/Svqk.svelte
@@ -1,68 +1,51 @@
 <script>
   import { Carousel, P, Tabs, TabItem } from 'flowbite-svelte';
-  // import { t } from '$lib/translations';
+  import { t } from '$lib/translations';
+  import WinGuide from '$lib/svqk/Win.svx';
+  import MacGuide from '$lib/svqk/Mac.svx';
 
   const images = [
     {
-      alt: 'jeg',
-      src: '/jpa-entity-generator.svg',
-      title: 'jeg',
+      alt: 'setup',
+      src: '/svqk/setup.png',
+      title: 'setup',
     },
     {
-      alt: 'csv',
-      src: '/batch-translator.svg',
-      title: 'csv',
+      alt: 'pw-repo',
+      src: '/svqk/pw-repo.png',
+      title: 'pw-repo',
+    },
+    {
+      alt: 'vscode-ws',
+      src: '/svqk/vscode-ws.png',
+      title: 'vscode-ws',
+    },
+    {
+      alt: 'regist-issue',
+      src: '/svqk/regist-issue.png',
+      title: 'regist-issue',
     },
   ];
-
-  const command = {
-    win: `
-chcp 65001
-
-mvn archetype:generate ^
-  -DarchetypeGroupId=dev.aulait.svqk ^
-  -DarchetypeArtifactId=svqk-archetype-refimpl ^
-  -DarchetypeVersion=0.8 ^
-  -DgroupId=my-groupId ^
-  -DartifactId=my-artifactId
-
-cd my-artifactId
-
-mvnw install -T 1C -P setup
-
-code my-artifactId.code-workspace
-    `,
-    mac: `
-mvn archetype:generate \\
-  -DarchetypeGroupId=dev.aulait.svqk \\
-  -DarchetypeArtifactId=svqk-archetype-refimpl \\
-  -DarchetypeVersion=0.8 \\
-  -DgroupId=my-groupId \\
-  -DartifactId=my-artifactId
-
-cd my-artifactId
-
-./mvnw install -T 1C -P setup
-
-code my-artifactId.code-workspace
-    `,
-  };
 </script>
 
 <div class="container mx-auto lg:w-4/5">
-  <P size="6xl">SVQK</P>
-  <P size="4xl">Features</P>
-  <P size="4xl">Quick Start</P>
+  <P size="4xl">SVQK</P>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  <p class="text-gray-600">{@html $t('msg.products.SVQK.feature')}</p>
+
+  <P size="2xl">Quick Start</P>
+
   <Tabs>
     <TabItem open title="Windows">
-      <pre><code>{command.win}</code></pre>
+      <WinGuide />
     </TabItem>
     <TabItem title="Mac">
-      <pre><code>{command.mac}</code></pre>
+      <MacGuide />
     </TabItem>
   </Tabs>
 
-  <div>
-    <Carousel {images}  duration="5000" />
+  <div class="max-w-4xl mx-auto">
+    <!-- TODO 高さが固定値指定以外で拡張できない -->
+    <Carousel {images} duration={5000} style="height: 32rem;" />
   </div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), mdsvex()],
 
   kit: {
     adapter: adapter({
@@ -18,6 +19,8 @@ const config = {
     // 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     // }
   },
+
+  extensions: ['.svelte', '.svx'],
 };
 
 export default config;
