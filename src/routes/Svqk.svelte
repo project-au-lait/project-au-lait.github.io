8a5792fdea6ebfd9188ffed185daba93647a0454
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-08-23 16:33:42 +0900

    wip rm mdsvex add highlight

--- src/routes/Svqk.svelte
+++ src/routes/Svqk.svelte
@@ -1,8 +1,10 @@
 <script>
-  import { Carousel, P, Tabs, TabItem } from 'flowbite-svelte';
   import { t } from '$lib/translations';
-  import WinGuide from '$lib/svqk/Win.svx';
-  import MacGuide from '$lib/svqk/Mac.svx';
+  import { Carousel, Kbd, TabItem, Tabs } from 'flowbite-svelte';
+  import { CodeCopy } from 'svelte-code-copy';
+  import Highlight from 'svelte-highlight';
+  import { bash, dos } from 'svelte-highlight/languages';
+  import monokai from 'svelte-highlight/styles/monokai';
 
   const images = [
     {
@@ -26,26 +28,114 @@
       title: 'regist-issue',
     },
   ];
+
+  const qsStepClass = 'text-lg my-2';
 </script>
 
-<div class="container mx-auto lg:w-4/5">
-  <P size="4xl">SVQK</P>
-  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-  <p class="text-gray-600">{@html $t('msg.products.SVQK.feature')}</p>
+<!-- eslint-disable svelte/no-at-html-tags -->
+<svelte:head>
+  {@html monokai}
+</svelte:head>
 
-  <P size="2xl">Quick Start</P>
+<div class="container mx-auto lg:w-4/5">
+  <p class="text-4xl title-font font-medium my-2">SVQK</p>
+  <p class="text-gray-600 mb-4">{@html $t('msg.products.SVQK.feature')}</p>
+
+  <p class="text-2xl my-2">Quick Start</p>
+  <p class="mb-2">{$t('msg.products.SVQK.usage.description')}</p>
 
   <Tabs>
     <TabItem open title="Windows">
-      <WinGuide />
+      <p class={qsStepClass}>1. {$t('msg.products.SVQK.usage.setUtf-8')}</p>
+      <CodeCopy><Highlight language={dos} code={'chcp 65001'} /></CodeCopy>
+
+      <p class={qsStepClass}>2. {$t('msg.products.SVQK.usage.createPj')}</p>
+      <CodeCopy>
+        <Highlight
+          language={dos}
+          code={`mvn archetype:generate ^
+  -DarchetypeGroupId=dev.aulait.svqk ^
+  -DarchetypeArtifactId=svqk-archetype-refimpl ^
+  -DarchetypeVersion=0.8 ^
+  -DgroupId=my-groupId ^
+  -DartifactId=my-artifactId`}
+        />
+      </CodeCopy>
+
+      <p class={qsStepClass}>3. {$t('msg.products.SVQK.usage.setup')}</p>
+      <CodeCopy><Highlight language={dos} code={'cd my-artifactId'} /></CodeCopy>
+      <CodeCopy><Highlight language={dos} code={'mvnw install -T 1C -P setup'} /></CodeCopy>
+
+      <p class={qsStepClass}>4. {$t('msg.products.SVQK.usage.openWs')}</p>
+      <CodeCopy><Highlight language={dos} code={'code my-artifactId.code-workspace'} /></CodeCopy>
+
+      <p class={qsStepClass}>5. {$t('msg.products.SVQK.usage.runTask')}</p>
+      <p class={qsStepClass}>
+        <Kbd class="px-2 py-1.5">Ctrl</Kbd> + <Kbd class="px-2 py-1.5">Shift</Kbd> + <Kbd
+          class="px-2 py-1.5">P</Kbd
+        > &gt; Tasks: Run task &gt; start-back
+      </p>
+      <p class={qsStepClass}>
+        <Kbd class="px-2 py-1.5">Ctrl</Kbd> + <Kbd class="px-2 py-1.5">Shift</Kbd> + <Kbd
+          class="px-2 py-1.5">P</Kbd
+        > &gt; Tasks: Run task &gt; start-front
+      </p>
+
+      <p class={qsStepClass}>6. {$t('msg.products.SVQK.usage.access')}</p>
+      <p class={qsStepClass}>
+        <a
+          href="http://localhost:5173"
+          class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >http://localhost:5173</a
+        >
+      </p>
     </TabItem>
+
     <TabItem title="Mac">
-      <MacGuide />
+      <p class={qsStepClass}>1. {$t('msg.products.SVQK.usage.createPj')}</p>
+      <CodeCopy>
+        <Highlight
+          language={bash}
+          code={`mvn archetype:generate \\
+  -DarchetypeGroupId=dev.aulait.svqk \\
+  -DarchetypeArtifactId=svqk-archetype-refimpl \\
+  -DarchetypeVersion=0.8 \\
+  -DgroupId=my-groupId \\
+  -DartifactId=my-artifactId`}
+        />
+      </CodeCopy>
+
+      <p class={qsStepClass}>2. {$t('msg.products.SVQK.usage.setup')}</p>
+      <CodeCopy><Highlight language={bash} code={'cd my-artifactId'} /></CodeCopy>
+      <CodeCopy><Highlight language={bash} code={'./mvnw install -T 1C -P setup'} /></CodeCopy>
+
+      <p class={qsStepClass}>3. {$t('msg.products.SVQK.usage.openWs')}</p>
+      <CodeCopy><Highlight language={bash} code={'code my-artifactId.code-workspace'} /></CodeCopy>
+
+      <p class={qsStepClass}>4. {$t('msg.products.SVQK.usage.runTask')}</p>
+      <p class={qsStepClass}>
+        <Kbd class="px-2 py-1.5">⌘ Command</Kbd> + <Kbd class="px-2 py-1.5">⇧ Shift</Kbd> + <Kbd
+          class="px-2 py-1.5">P</Kbd
+        > &gt; Tasks: Run task &gt; start-back
+      </p>
+      <p class={qsStepClass}>
+        <Kbd class="px-2 py-1.5">⌘ Command</Kbd> + <Kbd class="px-2 py-1.5">⇧ Shift</Kbd> + <Kbd
+          class="px-2 py-1.5">P</Kbd
+        > &gt; Tasks: Run task &gt; start-front
+      </p>
+
+      <p class={qsStepClass}>5. {$t('msg.products.SVQK.usage.access')}</p>
+      <p class={qsStepClass}>
+        <a
+          href="http://localhost:5173"
+          class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >http://localhost:5173</a
+        >
+      </p>
     </TabItem>
   </Tabs>
 
   <div class="max-w-4xl mx-auto">
-    <!-- TODO 高さが固定値指定以外で拡張できない -->
     <Carousel {images} duration={5000} style="height: 32rem;" />
   </div>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [vitePreprocess(), mdsvex()],
+  preprocess: [vitePreprocess()],
 
   kit: {
     adapter: adapter({
@@ -19,8 +18,6 @@ const config = {
     // 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     // }
   },
-
-  extensions: ['.svelte', '.svx'],
 };
 
 export default config;
