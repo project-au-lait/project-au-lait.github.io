74d85232d95821534aac6dc7a5b050f969104ac9
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-03-28 14:46:56 +0900

    fix: align header right-side items evenly and improve dropdown width

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -32,29 +32,35 @@
       </div>
     </a>
 
-    <div class="flex items-center gap-4">
-      <div>
-        <Button>
-          <GlobeSolid size="lg" />
-          <ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" />
-        </Button>
-        <Dropdown bind:open={dropdownOpen}>
-          {#each $locales as localeValue}
-            <DropdownItem on:click={() => chooseItem(localeValue)}>
-              {localeLabels[localeValue]}
-            </DropdownItem>
-          {/each}
-        </Dropdown>
-      </div>
-      <div>
-        <a href="https://github.com/project-au-lait">
-          <img src="./github-mark-white.svg" alt="GitHub" class="h-8" />
-        </a>
-      </div>
-      <div>
-        <a href={xLink} target="_blank" rel="noopener noreferrer">
-          <img src="./x-logo-white.png" alt="X" class="h-5" />
-        </a>
+    <div class="flex justify-end items-center">
+      <div class="flex items-center justify-between w-[12rem]">
+        <div class="w-12 flex justify-center relative">
+          <div class="w-full">
+            <Button class="w-full !p-0 flex items-center justify-center h-10">
+              <GlobeSolid size="lg" />
+              <ChevronDownOutline class="w-5 h-5 ms-1 text-white dark:text-white" />
+            </Button>
+            <Dropdown bind:open={dropdownOpen} class="w-full mt-2 z-50">
+              {#each $locales as localeValue}
+                <DropdownItem class="text-center" on:click={() => chooseItem(localeValue)}>
+                  {localeLabels[localeValue]}
+                </DropdownItem>
+              {/each}
+            </Dropdown>
+          </div>
+        </div>
+
+        <div class="w-12 flex justify-center">
+          <a href="https://github.com/project-au-lait">
+            <img src="./github-mark-white.svg" alt="GitHub" class="h-8" />
+          </a>
+        </div>
+
+        <div class="w-12 flex justify-center">
+          <a href={xLink} target="_blank" rel="noopener noreferrer">
+            <img src="./x-logo-white.png" alt="X" class="h-5" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
